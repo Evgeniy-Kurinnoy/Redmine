ae086c0aa780dcdd7d789f8e37e0b3f49b7586a4
Evgeniy Kurinnoy <ekurinnoy@gmail.com>
2019-07-04 13:21:38 +0300

    Initial Commit

---
+++ Redmine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Redmine
+//
+//  Created by Евгений Куринной on 7/4/19.
+//  Copyright © 2019 Evgeniy Kurinnoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
